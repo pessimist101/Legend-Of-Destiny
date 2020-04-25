c242b330c4c9ab70f9c5dd450e55112766d49b1b
Evie Snuggle <^-^@Snugg.ie>
2020-04-26 00:12:42 +0100

    Add comments to reaction code!

--- cogs/textAdventure.py
+++ cogs/textAdventure.py
@@ -67,23 +67,28 @@ class TextAdventure(commands.Cog):
         cursor.close()
         connection.close()
 
-        stats_list = ['armour', 'agility', 'attack', 'magic']
-        number_dict = {'1️⃣': 'one', '2️⃣': 'two', '3️⃣': 'three', '4️⃣': 'four', '5️⃣': 'five'}
-        # part 2 the reckoning
-        for stat in stats_list:
-            embedObject = discord.Embed(colour=discord.Colour(0xdbc036), description=f"How much {stat} do you have, <@{ctx.author.id}>?")
-            messageObject = await ctx.send(embed=embedObject)
+        ### Time to pick people's stats! ###
+        stats_list = ['armour', 'agility', 'attack', 'magic'] # What stats to include?
+        number_dict = {'1️⃣': 'one', '2️⃣': 'two', '3️⃣': 'three', '4️⃣': 'four', '5️⃣': 'five'} # What is each emoji called?
 
+        for stat in stats_list: # For every stat in the list, make a message asking how much people have...
+            embedObject = discord.Embed(colour=discord.Colour(0xdbc036), description=f"How much {stat} do you have, <@{ctx.author.id}>?")
+            messageObject = await ctx.send(embed=embedObject) # Send that message.
+
+            # For every single emoji in the number_dict, add a reaction to the prior message.
             for emoji in number_dict:
                 await messageObject.add_reaction(f"{emoji}")
 
+            # Predicate/condition to be used later. Check the user is the original author and it's the same message.
             def reaction_info_check(reaction, user):
                 return user == ctx.author and reaction.message.id == messageObject.id
 
+            # Pause/wait for the user to react with an emoji that meets the above conition.
             reaction, user = await self.client.wait_for('reaction_add', timeout=30.0, check=reaction_info_check)
-            for emoji in number_dict:
-                if reaction.emoji == emoji:
-                    await ctx.send(f"You have selected {number_dict[emoji]} points in your {stat} stat.")
+
+            # Okay, the user has reacted with an emoji, let us find out which one!
+            if reaction.emoji in number_dict:
+                await ctx.send(f"You have selected {number_dict[reaction.emoji]} points in your {stat} stat.")
 
         await self.mystats.callback(self=self, ctx=ctx)
 
